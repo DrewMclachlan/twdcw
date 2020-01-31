697234c5df61a2d1ceee5d83a339f2e37ab35987
Drew <drew.mclachlan@hotmail.co.uk>
2020-01-31 00:28:52 +0000

    chapter 7- progress

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Category
 from .models import Page
+from .forms import CategoryForm
+from django.shortcuts import redirect
+
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -30,3 +33,16 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = None
 
     return render(request, 'rango/category.html', context=context_dict)
+
+def add_category(request):
+    form = CategoryForm()
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return redirect('/rango/')
+        else:
+            print(form.errors)
+    return render(request, 'rango/add_category.html', {'form': form})
+
